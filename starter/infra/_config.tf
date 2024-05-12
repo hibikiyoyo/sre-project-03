7a5475fe8be2de602342d194140cfa0cac13a309
hibikiyoyo <hibikiyoyo.trung@gmail.com>
2024-05-12 18:34:25 +0700

    udacity

--- starter/infra/_config.tf
+++ starter/infra/_config.tf
@@ -1,13 +1,15 @@
 terraform {
   backend "s3" {
-    bucket = "udacity-tf-<first_name>" # Update here with your S3 bucket
+    bucket = "udacity-tf-trungdd22" # Update here with your S3 bucket
     key    = "terraform/terraform.tfstate"
     region = "us-east-2"
+    profile = "udacity"
   }
 }
 
 provider "aws" {
   region = "us-east-2"
+  profile = "udacity" # lets say profile is my-profile
 
   default_tags {
     tags = local.tags
--- starter/infra/kubernetes_resources.tf
+++ starter/infra/kubernetes_resources.tf
@@ -1,11 +1,11 @@
-resource "kubernetes_namespace" "udacity" {
-  metadata {
-    name = local.name
-  }
-  depends_on = [
-    module.project_eks
-  ]
-}
+# resource "kubernetes_namespace" "udacity" {
+#   metadata {
+#     name = local.name
+#   }
+#   depends_on = [
+#     module.project_eks
+#   ]
+# }
 
 resource "kubernetes_service" "blue" {
   metadata {
@@ -19,6 +19,33 @@ resource "kubernetes_service" "blue" {
   spec {
     selector = {
       app = "blue"
+    }
+
+    port {
+      port        = 80
+      target_port = 80
+    }
+
+    type = "LoadBalancer"
+  }
+
+  depends_on = [
+    module.project_eks
+  ]
+}
+
+resource "kubernetes_service" "green" {
+  metadata {
+    name      = "green-svc"
+    namespace = local.name
+    annotations = {
+      "service.beta.kubernetes.io/aws-load-balancer-type"            = "nlb"
+      "service.beta.kubernetes.io/aws-load-balancer-nlb-target-type" = "ip"
+    }
+  }
+  spec {
+    selector = {
+      app = "green"
     }
 
     port {
